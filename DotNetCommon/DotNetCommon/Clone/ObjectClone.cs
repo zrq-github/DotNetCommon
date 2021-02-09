ef0c999f61f7d7c7999e8e5f225edf156464537d
zrq <583175160@qq.com>
2021-02-09 17:30:37 +0800

    1

---
+++ DotNetCommon/DotNetCommon/Clone/ObjectClone.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCommon.Clone
+{
+    /// <summary>
+    /// 对类进行Clone
+    /// </summary>
+    /// <remarks>序列化克隆</remarks>
+    public static class ObjectClone
+    {
+        /// <summary>
+        /// 序列化克隆类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">Thrown when object is not be serializable</exception>
+        public static T CloneSerialize<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
